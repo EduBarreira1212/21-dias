271db132d819c98cb037493acd47e76e199bfa3e
Eduardo Barreira <edubarreira1212@gmail.com>
2023-08-20 20:17:09 -0300

    Day 6 - FOR loop challengers

---
+++ Dia 6/Challenge1.js
@@ -0,0 +1,19 @@
+//Exercise 1
+let number = parseInt(prompt("Insert a number positive and integer"));
+
+for(let i = 0; i<=number; i++){
+    console.log(i);
+}
+console.log("=====================");
+
+//Exercise 2
+for (let i = 0; i <= 50; i+=5) {
+    console.log(i);
+}
+console.log("Fim!");
+console.log("=====================");
+
+for (let i = 50; i >= 0; i-=5) {
+    console.log(i);
+}
+console.log("Fim!");
